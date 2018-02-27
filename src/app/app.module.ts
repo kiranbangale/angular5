fc0e61f3b07423cd8f95c3fb445900f46c2e7ffb
Kiran Bangale <kiranbangale@Kirans-MacBook-Pro.local>
2018-02-27 22:46:54 +0530

    Added carousel and bar chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgxCarouselModule } from 'ngx-carousel';
+import 'hammerjs';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+// import { WeatherService } from './services/weather.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +19,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    NgxCarouselModule
   ],
-  providers: [],
+  providers: [
+    // WeatherService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
