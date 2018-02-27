482d7cf54414e850afaea900e97ae8e1f7783993
Kiran Bangale <kiranbangale@Kirans-MacBook-Pro.local>
2018-02-27 23:08:31 +0530

    Added public method for charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -94,5 +94,9 @@ export class AppComponent implements OnInit{
       } 
     }); 
 } 
-
+public myfunc(event: Event) {
+     // carouselLoad will trigger this funnction when your load value reaches
+     // it is helps to load the data by parts to increase the performance of the app
+     // must use feature to all carousel
+  }
 }
